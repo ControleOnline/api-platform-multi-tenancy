48192ad9bf4eb2f53dc0f84cd80796974a1bc8eb
Luiz Kim <luiz.kim@controleonline.com>
2024-01-06 10:24:44 -0300

    Command

---
+++ src/Command/MigrateCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ControleOnline\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Migrations\Tools\Console\Command\MigrateCommand as Migrate;
+
+#[AsCommand(
+    name: 'tenant:migrations:migrate',
+    description: 'Proxy to migrate a new tenant database.',
+)]
+final class MigrateCommand extends Command
+{
+
+    protected $em;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em     = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName('tenant:migrations:migrate')
+            ->setAliases(['t:m:m'])
+            ->setDescription('Proxy to launch doctrine:migrations:migrate for specific database .')
+            ->addArgument('domain', InputArgument::OPTIONAL, 'Database Domain Identifier')
+            ->addArgument('version', InputArgument::OPTIONAL, 'The version number (YYYYMMDDHHMMSS) or alias (first, prev, next, latest) to migrate to.', 'latest')
+            ->addOption('dry-run', null, InputOption::VALUE_NONE, 'Execute the migration as a dry run.')
+            ->addOption('query-time', null, InputOption::VALUE_NONE, 'Time all the queries individually.')
+            ->addOption('allow-no-migration', null, InputOption::VALUE_NONE, 'Do not throw an exception when no changes are detected.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $newInput = new ArrayInput([
+            'version' => $input->getArgument('version'),
+            '--dry-run' => $input->getOption('dry-run'),
+            '--query-time' => $input->getOption('query-time'),
+            '--allow-no-migration' => $input->getOption('allow-no-migration'),
+        ]);
+        $newInput->setInteractive($input->isInteractive());
+        $otherCommand = new Migrate();
+        $otherCommand->run($newInput, $output);
+
+        return 0;
+    }
+}
