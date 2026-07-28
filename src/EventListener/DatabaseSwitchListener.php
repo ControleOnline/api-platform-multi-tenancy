64e07060158296c5cb44af514a06586901b8d37e
Luiz Kim <luiz.kim@controleonline.com>
2026-07-28 20:51:37 -0300

    Switch tenancy on every request

--- src/EventListener/DatabaseSwitchListener.php
+++ src/EventListener/DatabaseSwitchListener.php
@@ -9,10 +9,6 @@ use Exception;
 
 class DatabaseSwitchListener
 {
-    private $domain;
-    private static $tenancy_params;
-
-
     public function __construct(
         private DatabaseSwitchService $databaseSwitchService,
         private DomainService $domainService
@@ -21,9 +17,8 @@ class DatabaseSwitchListener
     public function onKernelRequest(RequestEvent $event)
     {
         try {
-            
-            if (!self::$tenancy_params && $_ENV['MULTI_TENANCY'])
-                self::$tenancy_params = $this->databaseSwitchService->switchDatabaseByDomain(
+            if ($_ENV['MULTI_TENANCY'])
+                $this->databaseSwitchService->switchDatabaseByDomain(
                     $this->domainService->getDomain()
                 );
         } catch (Exception $e) {
